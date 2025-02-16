43669eaf175040aeee0c2c69514989ea36cd24e5
Mateus Camacho <teukuka.silva@hotmail.com>
2025-02-16 17:33:47 -0300

    coisas
    coisas

--- pacientes/views.py
+++ pacientes/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Pacientes
+from django.contrib import messages
+from django.contrib.messages import constants
 # Create your views here.
 def pacientes(request):
     #isso aqui ele fala basicamente que quando chamar essa função vai rodar o html
-    queixas = Pacientes.queixa_choices
-    
-    return render(request, 'pacientes.html', {queixas: queixas})
+    if request.method == "GET":
+        queixas = Pacientes.queixa_choices
+        return render(request, 'pacientes.html', {'queixas': queixas})
+    elif request.method == "POST":
+
+
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        telefone = request.POST.get('telefone')
+        queixa = request.POST.get('queixa')
+        foto = request.FILES.get('foto')
+        
+        if len(nome.strip()) == 0 or not foto:
+            
+            return redirect('pacientes')
+        
+        paciente = Pacientes(
+            nome = nome,
+            email=email,
+            telefone=telefone,
+            queixa=queixa,
+            foto=foto
+        )
+        paciente.save()
+
+        return redirect('pacientes')
